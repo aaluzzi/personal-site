ef8998ffcafab853e32383bc8d08e13f550e9a66
Austin Aluzzi <austinaluzzi@gmail.com>
2024-04-22 15:24:22 -0700

    Add project images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
         <section className="bg-slate-800 flex-grow flex flex-col items-center justify-center gap-8">
             <h1 className="text-6xl font-bold leading-tight">Hey, I'm Austin 👋</h1>
-            <p className="text-3xl">Lifelong Insulin Addict</p>
+            <p className="text-3xl">Full Stack Developer</p>
         </section>
     )
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,10 +3,10 @@ import Button from "./Button";
 export default function Project({ project }) {
     return (
         <div className="max-w-[360px] bg-slate-800 rounded-lg p-8 flex flex-col gap-4 drop-shadow-md">
-            <div className="bg-slate-400 aspect-video"></div>
+            <img src={`./images/${project.id}.png`} className="bg-slate-400 aspect-video" />
             <h1 className="text-2xl font-bold">{project.name}</h1>
             <p className="italic">Built With: {getTechnologiesString(project.technologies)}</p>
-            <p>{project.description}</p>
+            <p className="flex-grow flex items-center">{project.description}</p>
             <div className="flex gap-4 justify-center">
                 {ProjectButtons(project.demo, project.code)}
             </div>
