2c2ff0889012ab2b906120d765431e5fa3a2433d
Austin Aluzzi <austinaluzzi@gmail.com>
2024-04-25 14:37:25 -0700

    Reduce project image sizes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ export default function About() {
         <section id="about-me" className="py-16 px-8 flex flex-col items-center justify-center gap-4">
             <h1 className="text-3xl font-bold">About Me</h1>
             <p className="max-w-[720px] text-lg bg-slate-800 p-4 rounded-md">
-                Im currently a senior studying computer science at California State University, Pomona. My programming journey began with Java, 
+                I'm currently a senior studying computer science at California State University, Pomona. My programming journey began with Java, 
                 with my curriculum primarily focused on object-oriented languages and design of algorithms. 
-                However, my interest in the versatility of the web led me to explore it further.
+                However, discovering the versatility of the web led me to explore it myself.
                 Since then, I've enjoyed delving into technologies spanning from frontend to backend and constructing full-stack solutions that stand on their own.
             </p>
         </section>
