46f3c70e9c102430edb9bdcd6c9cf53151ae348c
Austin Aluzzi <austinaluzzi@gmail.com>
2024-04-25 14:37:16 -0700

    Add hero section animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     return (
         <section className="bg-slate-800 flex-grow flex flex-col items-center justify-center gap-8">
-            <h1 className="text-6xl font-bold leading-tight">Hey, I'm Austin 👋</h1>
-            <p className="text-3xl">Full Stack Developer</p>
+            <h1 className="text-6xl font-bold leading-tight animate-rise1">Hey, I'm Austin <span className="inline-block animate-wave origin-bottom-right">👋</span></h1>
+            <p className="text-3xl animate-rise2">Full Stack Developer</p>
         </section>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wave: {
+          '16.67%, 83.33%': { transform: 'rotate(-10deg)' },
+          '50%': { transform: 'rotate(10deg)'}
+        },
+        rise: {
+          '0%': {
+            transform: 'translateY(150px)',
+            opacity: 0,
+            },
+          '50%': {
+            opacity: 0
+          }
+        }
+      },
+      animation: {
+        wave: '1.25s wave 0.75s ease-in-out',
+        rise1: 'rise 0.75s ease-in-out',
+        rise2: 'rise 1.5s ease-in-out'
+      }
+    },
   },
   plugins: [],
 }
