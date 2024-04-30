acbee1359a8d022377642a38982002c88b59bc78
Austin Aluzzi <austinaluzzi@gmail.com>
2024-04-29 19:35:30 -0700

    Try to fix button icons not displaying on mobile

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -25,8 +25,8 @@ export default function Project({ project }) {
 function ProjectButtons(demo, code) {
     return (
         <>
-            <Button className="bg-green-700 hover:bg-green-800" icon={<ComputerDesktopIcon className="h-[100%]" />} label="Demo" link={demo} />
-            <Button className="bg-indigo-700 hover:bg-indigo-800" icon={<CodeBracketIcon className="h-[100%]" />} label="Code" link={code} />
+            <Button className="bg-green-700 hover:bg-green-800" icon={<ComputerDesktopIcon className="h-6 w-6" />} label="Demo" link={demo} />
+            <Button className="bg-indigo-700 hover:bg-indigo-800" icon={<CodeBracketIcon className="h-6 w-6" />} label="Code" link={code} />
         </>
     )
 }
