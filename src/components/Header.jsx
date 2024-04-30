f4b7746b0089c6dca5e2185f9b0749e74a847f4e
Austin Aluzzi <austinaluzzi@gmail.com>
2024-04-30 13:26:14 -0700

    Display header icons instead of text on small displays

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
+import { ComputerDesktopIcon, LinkIcon, UserIcon } from "@heroicons/react/24/solid";
+
 export default function Header() {
     return (
         <header className="bg-slate-700 drop-shadow-md">
           <nav className="mx-auto max-w-[1280px] flex-grow flex justify-between p-4">
             <div className="text-xl font-bold">Austin Aluzzi</div>
             <ul className="flex items-center justify-center gap-6">
-              <li><a href="#about-me">About Me</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
-              <li><a href="#connect">Connect</a></li>
+              <li><a href="#about-me"><UserIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block">About Me</span></a></li>
+              <li><a href="#portfolio"><ComputerDesktopIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block">Portfolio</span></a></li>
+              <li><a href="#connect"><LinkIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block">Connect</span></a></li>
             </ul>
           </nav>
         </header>
