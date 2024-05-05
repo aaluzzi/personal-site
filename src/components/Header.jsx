764b44ff8553d55d76f05f42e9ba01efdc449153
Austin Aluzzi <austinaluzzi@gmail.com>
2024-05-04 21:09:37 -0700

    Remove Connect from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { ComputerDesktopIcon, LinkIcon, UserIcon } from "@heroicons/react/24/solid";
+import { ComputerDesktopIcon, UserIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
     return (
@@ -6,9 +6,9 @@ export default function Header() {
           <nav className="mx-auto max-w-[1280px] flex-grow flex justify-between p-4">
             <div className="text-xl font-bold">Austin Aluzzi</div>
             <ul className="flex items-center justify-center gap-6">
-              <li><a href="#about-me"><UserIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block">About Me</span></a></li>
-              <li><a href="#portfolio"><ComputerDesktopIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block">Portfolio</span></a></li>
-              <li><a href="#connect"><LinkIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block">Connect</span></a></li>
+              <li><a href="#about-me"><UserIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block font-bold hover:underline">About Me</span></a></li>
+              <li><a href="#portfolio"><ComputerDesktopIcon className="h-6 w-6 block md:hidden"/><span className="hidden md:block font-bold hover:underline">Portfolio</span></a></li>
+      
             </ul>
           </nav>
         </header>
