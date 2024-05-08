a3c47233dde3c385f81ebc200feb84d2d7ca749f
Austin Aluzzi <austinaluzzi@gmail.com>
2024-05-08 14:54:23 -0700

    Update about, hero, and projects

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,12 +4,12 @@ export default function About() {
             <h1 className="text-4xl font-bold tracking-wide">About Me</h1>
             <p className="max-w-[720px] text-lg p-4">
                 My earliest memory of programming was trying to batch script a "Choose Your Own Adventure" game as a teen. 
-                It wasn't until a high school class that I understood what computer science was and my passion for it. 
+                It wasn't until a high school class that I learned what computer science was and found my passion for it. 
                 <br/><br/>
-                College has taught me formal topics like object-oriented paradigms and algorithmic design. On the side however, 
-                I discovered the versatility of web development and brought my own ideas to life. I've learned many technologies spanning 
-                frontend to backend, and aspire to build more polished full-stack solutions in the future.
-                Away from programming, my interests include fitness, billiards, and piano.
+                Going through my college curriculum has educated me on many formal topics, but on the side, I discovered the versatility of web development.
+                Since then, I've brought my own ideas to life, learning several technologies spanning 
+                frontend to backend, and want to continue to pursue full stack solutions in the future.
+                Away from software developemnt, my interests include fitness, billiards, and piano.
             </p>
         </>
     )
