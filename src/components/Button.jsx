69956a1804092e07ee634a2de3eb348d8f54c7dd
Austin Aluzzi <austinaluzzi@gmail.com>
2024-05-08 14:59:20 -0700

    Prevent anchors from opening links in new tab

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({className, icon, label, link}) {
     return (
         <button className={"rounded-lg px-3 py-2 text-base font-medium drop-shadow-sm focus:outline-none focus-ring-4 focus-ring-indigo-300 " + className}>
-            <a className="flex gap-1 h-[100%]" href={link} target="_blank">
+            <a className="flex gap-1 h-[100%]" href={link}>
                 {icon}
                 {label}
             </a>
