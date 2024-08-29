794066edd5eb8789a7a24b4de86172657419d69b
Austin Aluzzi <austinaluzzi@gmail.com>
2024-08-28 23:13:46 -0700

    Add honeypot to contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,6 +4,7 @@ import Button from './Button';
 
 export default function ContactForm() {
     const [formData, setFormData] = useState({
+        name: '',
         email: '',
         message: '',
     });
@@ -27,11 +28,12 @@ export default function ContactForm() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email: formData.email, message: formData.message.trim() }),
+                body: JSON.stringify({ name: formData.name, email: formData.email, message: formData.message.trim() }),
             });
             const json = await resp.json();
             setStatus(json.message);
             setFormData({
+                name: '',
                 email: '',
                 message: '',
             });
@@ -43,12 +45,22 @@ export default function ContactForm() {
     return (
         <form className="w-full max-w-[512px] flex flex-col items-center gap-4 rounded-md" onSubmit={handleSubmit}>
             <input
+                className="w-full h-0 bg-stone-900 rounded-lg shadow-md focus:bg-stone-800 focus:outline-none"
+                placeholder="Your name"
+                type="name"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+            />
+            <input
                 className="w-full bg-stone-900 p-3 rounded-lg shadow-md focus:bg-stone-800 focus:outline-none"
                 placeholder="Your email"
                 type="email"
                 id="email"
                 name="email"
                 value={formData.email}
+                maxLength="32"
                 onChange={handleChange}
                 required
             />
@@ -58,6 +70,7 @@ export default function ContactForm() {
                 id="message"
                 name="message"
                 value={formData.message}
+                maxLength="1024"
                 onChange={handleChange}
                 required
             ></textarea>
